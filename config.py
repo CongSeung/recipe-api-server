4fdd8e400f62c1131a43137bb26881ed69d5a9cb
CongSeung <leem1121@gmail.com>
2022-06-21 10:52:42 +0900

    new

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config :
     # 이 부분은 시드 값으로 외부 유출 XX
     JWT_SECRET_KEY = 'highhhhrqllzq1206'
 
-    # True는 기본적으로 토큰 유지 시간제한 180초, False 시간제한 없음
+    # True는 토큰 유지 시간제한을 원할 때, False 시간제한 없음
     JWT_ACCESS_TOKEN_EXPIRES = False
 
     PROPAGATE_EXEPTIONS = True
